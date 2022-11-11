6d92b1da9dae3eace6f3de89f1de1e57acb5275c
MiguelSerralde <85590150+MiguelSerralde@users.noreply.github.com>
2022-11-10 19:54:05 -0700

    UpdateLoginCorrect

--- js/clase.js
+++ js/clase.js
@@ -2,13 +2,30 @@ const fornmDom = document.getElementById('formulario')
 const userbd = 'Miguel'
 const passbd = '123'
 
+var cuentas = [
+    {nombre: 'Mali', paswword: '123', saldo: 200},
+    {nombre: 'Gera', paswword: '123', saldo: 290},
+    {nombre: 'Maui', paswword: '123', saldo: 67}
+]
+
+console.log(cuentas[0].nombre)
+
 function login(userDom, passDom){
-    if (userbd === userDom && passbd === passDom){
-        alert('Acceso Correcto')
+    for (i = 0; i <= 2; i++){
+        if (cuentas[i].nombre == userDom && cuentas[i].paswword == passDom){
+            alert("Login correcto")
+        }
+        else {
+            mostrarError()
+        }
     }
-    else {
-        mostrarError()
-    }
+
+    // if (userbd === userDom && passbd === passDom){
+    //     alert('Acceso Correcto')
+    // }
+    // else {
+    //     mostrarError()
+    // }
 }
 
 function mostrarError(){
